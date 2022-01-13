cd1b07b242d2261cdf51a70a9425274b0699edc5
Andreas Eliasson <andreaseliasson91@gmail.com>
2022-01-13 13:30:34 +0100

    added database structure

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,4 @@
+from application.dll.db import init_db
 from flask import Flask
 from flask_login import LoginManager
 
@@ -5,6 +6,7 @@ from flask_login import LoginManager
 def create_app():
     _app = Flask(__name__)
     _app.config.from_pyfile('settings.py')
+    init_db(_app)
 
     login_manager = LoginManager()
     login_manager.init_app(_app)
---
+++ application/dll/models/__init__.py
@@ -0,0 +1,20 @@
+from application.dll.db import db, Document
+
+
+class User(Document):
+    collection = db.users
+
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    def get_id(self):
+        return self.username
