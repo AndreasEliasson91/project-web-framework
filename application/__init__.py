c43317c04f273493ab21cba66acb024f888c9af7
Andreas Eliasson <andreaseliasson91@gmail.com>
2022-01-14 09:30:58 +0100

    added template-directories to blueprints. Added a debug toolbar

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,6 @@
 from application.dll.db import init_db
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 from flask_login import LoginManager
 
 
@@ -7,6 +8,8 @@ def create_app():
     _app = Flask(__name__)
     _app.config.from_pyfile('settings.py')
     init_db(_app)
+
+    toolbar = DebugToolbarExtension(_app)
 
     login_manager = LoginManager()
     login_manager.init_app(_app)
