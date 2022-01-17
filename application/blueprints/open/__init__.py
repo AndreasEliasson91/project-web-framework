0543a5b83f69c0678b058bd20e4651f7ee101d55
Andreas Eliasson <andreaseliasson91@gmail.com>
2022-01-17 10:26:20 +0100

    created open blueprint

---
+++ application/blueprints/open/__init__.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template, redirect, request, url_for
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import check_password_hash
+
+
+bp_open = Blueprint('bp_open', __name__)
+
--- application/dll/db/__init__.py
+++ application/dll/db/__init__.py
@@ -12,11 +12,11 @@ def init_db(app):
     :return: None
     """
     global client, db
-    username = app.config["DB_USER"]
-    password = app.config["DB_PASSWORD"]
-    host = app.config["DB_HOST"]
-    port = int(app.config["DB_PORT"])
-    database = app.config["DB_NAME"]
+    username = app.config['DB_USER']
+    password = app.config['DB_PASSWORD']
+    host = app.config['DB_HOST']
+    port = int(app.config['DB_PORT'])
+    database = app.config['DB_NAME']
     client = MongoClient(f'mongodb://{username}:{password}@{host}:{port}')
     db = client[database]
 
