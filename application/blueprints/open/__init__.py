ff3c706ca5eddd576f803cf7b9d0ea6a86ac3d1d
lennyrydweissner <rydweissner@gmail.com>
2022-01-17 16:53:52 +0100

    First prototype of the homepage and navbar in place. commits by Anna och Lenny

--- application/blueprints/open/__init__.py
+++ application/blueprints/open/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, redirect, request, url_for
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import check_password_hash
 
-
 bp_open = Blueprint('bp_open',
                     __name__,
                     template_folder='templates'
@@ -12,3 +11,8 @@ bp_open = Blueprint('bp_open',
 @bp_open.get('/signup')
 def signup_get():
     return render_template('signup.html')
+
+
+@bp_open.get('/')
+def index():
+    return render_template('index.html')
