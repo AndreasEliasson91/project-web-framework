d5fa632c02eeb06127ff4cd76e5410e9f3d56ff6
lennyrydweissner <rydweissner@gmail.com>
2022-01-18 15:30:04 +0100

    Fixed all url_for so it is prepared for later use. Fixed Footer, it was blocking text, so you couldent se all information, we made it scrollable.
    Created a first prototype of the about page.
    made som changes in the base.html file.
    
    commits by Anna och Lenny

--- application/blueprints/open/__init__.py
+++ application/blueprints/open/__init__.py
@@ -35,8 +35,8 @@ def signin_post():
     return redirect(url_for('bp_open.index'))
 
 
-@bp_open.post('/signup')
-def signup_post():
+@bp_open.get('/signup')
+def signup_get():
     return render_template('signup.html')
 
 
