56559577a1d8bb5f5bf7b915f1645d02af0b317b
lennyrydweissner <rydweissner@gmail.com>
2022-01-19 17:11:27 +0100

    tried to fix some errors due to login. Comitts av Anna och Lenny.

--- application/__init__.py
+++ application/__init__.py
@@ -14,6 +14,9 @@ def create_app():
     from application.blueprints.open import bp_open
     _app.register_blueprint(bp_open)
 
+    from application.blueprints.users import bp_user
+    _app.register_blueprint(bp_user)
+
     from application.blueprints.parents import bp_parent
     _app.register_blueprint(bp_parent)
 
