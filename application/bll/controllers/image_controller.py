fc56207a723ebdef8f278911f52ca8308a1d8db8
Andreas Eliasson <andreaseliasson91@gmail.com>
2022-01-24 11:35:26 +0100

    fixed children profile picture. Moved code to image_repository

--- application/bll/controllers/image_controller.py
+++ application/bll/controllers/image_controller.py
@@ -7,3 +7,7 @@ def get_image(_id, filename):
 
 def get_all_image_is():
     return image_repository.get_all_image_ids()
+
+
+def upload_image(current_user, file):
+    return image_repository.upload_picture(current_user, file)
--- application/blueprints/users/__init__.py
+++ application/blueprints/users/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Blueprint, render_template, redirect, request, url_for, flash
 from flask_login import login_required, current_user
-from application.bll.controllers.image_controller import get_image
+from application.bll.controllers.image_controller import get_image, upload_image
 from application.dll.db import images
 from application.dll.db.models import Image, User
 
@@ -19,11 +19,6 @@ def user_index():
     return render_template('welcome.html')
 
 
-@bp_user.get('/profile')
-@login_required
-def profil_get():
-    return render_template('profil.html')
-
 @bp_user.get('/settings')
 @login_required
 def settings_get():
@@ -34,21 +29,25 @@ def settings_get():
 @login_required
 def contacts_get():
     return render_template('contacts.html')
- 
+
+
 @bp_user.get('/profile')
 @login_required
 def profile_get():
     if 'children' in current_user.__dict__:
-        children_pictures = []
+        children = []
         i = 0
 
         for child in current_user.children:
-            children_pictures.append(get_image(child['avatar'], f'child{i}'))
+            children.append({
+                'username': child['username'],
+                'avatar': get_image(child['avatar'], f'child{i}')
+            })
             i += 1
 
         return render_template('profile.html',
                                profile_picture=get_image(current_user.avatar, 'profile'),
-                               children_pictures=children_pictures)
+                               children=children)
     else:
         return render_template('profile.html',
                                profile_picture=get_image(current_user.avatar, 'profile'))
@@ -58,26 +57,5 @@ def profile_get():
 @login_required
 def profile_post():
     file = request.files.get('profile_picture')
-    file.save(os.path.join('application/static/img/temp/', 'temp.png'))
-
-    with open('application/static/img/temp/temp.png', 'rb') as in_file:
-        contents = in_file.read()
-
-    if Image.find(filename=file.filename).first_or_none() == None:
-        images.put(contents, filename=file.filename)
-    else:
-        i = 0
-        name = file.filename
-        for img in Image.all():
-            if img.filename == name:
-                i += 1
-                name = f'LBG{i}_{file.filename}'
-
-        images.put(contents, filename=f'{i}_{file.filename}')
-
-    image_id = Image.find(filename=file.filename).first_or_none()._id
-
-    current_user.avatar = image_id
-    User.save(current_user)
-
+    upload_image(current_user, file)
     return redirect(url_for('bp_user.profile_get'))
