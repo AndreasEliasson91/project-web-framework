db241c742e4794f5b85b02a02286b6ce6e6723e7
lennyrydweissner <rydweissner@gmail.com>
2022-02-04 14:23:23 +0100

    Working memory game, we added score and modified the game with other pictures and style. Commits by Anna and Lenny

--- application/static/js/memory_game.js
+++ application/static/js/memory_game.js
@@ -1,18 +1,131 @@
+class MixOrMatch {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.ticker = document.getElementById('flips');
+        this.score = document.getElementById('score');
+    }
 
+    startGame() {
+        // this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.cardToCheck = null;
+        this.matchedCards = [];
+        this.busy = true;
+        this.the_score = 0;
 
-// This is how we get a random item from an array.
+        this.score_string = ''
+        setTimeout(() => {
+            this.shuffleCards(this.cardsArray);
+            this.countdown = this.startCountdown();
+            this.busy = false;
+        }, 500)
+        this.hideCards();
+        this.timer.innerText = this.timeRemaining;
+        // this.ticker.innerText = this.totalClicks;
+        this.score.innerText = this.the_score;
+    }
+    startCountdown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0)
+                this.gameOver();
+        }, 1000);
+    }
+    gameOver() {
+        clearInterval(this.countdown);
+        document.getElementById('game-over-text').classList.add('visible');
+        this.the_score = 0;
+    }
+    victory() {
+        clearInterval(this.countdown);
+        document.getElementById('victory-text').classList.add('visible');
+    }
+    hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove('visible');
+            card.classList.remove('matched');
+        });
+    }
+    flipCard(card) {
+        if(this.canFlipCard(card)) {
+            this.totalClicks++;
+            // this.ticker.innerText = this.totalClicks;
+            card.classList.add('visible');
 
-function random_item(items)
-{
+            if(this.cardToCheck) {
+                this.checkForCardMatch(card);
+            } else {
+                this.cardToCheck = card;
+            }
+        }
+    }
+    checkForCardMatch(card) {
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck);
+        else
+            this.cardMismatch(card, this.cardToCheck);
 
-return items[Math.floor(Math.random()*items.length)];
-
+        this.cardToCheck = null;
+    }
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        this.the_score ++
+        this.score_string = this.the_score.toString()
+        this.score.innerText = this.score_string;
+        if(this.matchedCards.length === this.cardsArray.length)
+            this.victory();
+    }
+    cardMismatch(card1, card2) {
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible');
+            card2.classList.remove('visible');
+            this.busy = false;
+        }, 1000);
+    }
+    shuffleCards(cardsArray) { // Fisher-Yates Shuffle Algorithm.
+        for (let i = cardsArray.length - 1; i > 0; i--) {
+            let randIndex = Math.floor(Math.random() * (i + 1));
+            cardsArray[randIndex].style.order = i;
+            cardsArray[i].style.order = randIndex;
+        }
+    }
+    getCardType(card) {
+        return card.getElementsByClassName('card-value')[0].src;
+    }
+    canFlipCard(card) {
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    }
 }
 
-let items = ['Kanin', 'Mus', 'Sköldpadda', 'Flodhäst', 'räv', 'Kossa'];
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
 
-let items2 = ['mus.jpg', 'kanin.jpg', 'kossa.jpg', 'Sköldpadda.jpeg', 'räv.jpg', 'flodhäst.jpg']
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('big-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixOrMatch(100, cards);
 
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            game.startGame();
+        });
+    });
 
-console.log(random_item(items));
-console.log(random_item(items2));
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            game.flipCard(card);
+        });
+    });
+}
